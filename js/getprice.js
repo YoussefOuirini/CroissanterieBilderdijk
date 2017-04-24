a47cc1c834bdbf8cb8e95d187d99d36daf77a42b
Iuliia Glazeva <zhotter@mail.ru>
2017-04-24 17:25:27 +0200

    Changed blog

---
+++ js/getprice.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.someprice').on('keyup', '.quantity', function(){
+		
+		var price = +$(this).closest('.someprice').data('price');
+		var quantity = +$(this).val();
+		$('.total').text(price*quantity);
+
+	})
+})
+
+	
