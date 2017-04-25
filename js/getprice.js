9bbfda1d7350fbafb9a422f0e043d1996454238c
Iuliia Glazeva <zhotter@mail.ru>
2017-04-25 16:27:33 +0200

    Changed Price

--- js/getprice.js
+++ js/getprice.js
@@ -1,19 +1,10 @@
 $(document).ready(function(){
 	$('.someprice').on('keyup', function(){
-	
 		var price = +$(this).data('price');
 		var quantity = +$(this).find('.quantity').val();
-		
-		console.log(price)
-		console.log(quantity)
-
 		$(this).find("span").text("Total price: €" + price * quantity);
 	});
 	
 });
 
-function hey(){
-	var price = +$('#bla').closest('.someprice').data('price');
-	var quantity = +$('#bla').val();
-	$('#bla').children("span").text("boooya");
-}
+
