613c399249daab7f6ebcf25132657707307665e8
Noer <noerpaanakker@hotmail.com>
2017-05-10 14:26:26 +0200

    first try hosting locally

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use ('/', express.static('public'));
+
+app.get('/', (request, response) => {
+	response.render('index');
+});
+
+const listener = app.listen(3000, () => {
+	console.log('server has started at ', listener.address().port)
+});
